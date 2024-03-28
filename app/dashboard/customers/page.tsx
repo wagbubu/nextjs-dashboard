c20663c65dc9c0a50cc296f881a07e490826f272
raven <raven@pop-os.localdomain>
2024-03-28 15:37:51 +0800

    end of basic Next.JS tutorial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers, fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+  const customers = await fetchFilteredCustomers('');
+
+  return (
+    <>
+      <Table customers={customers}></Table>
+    </>
+  );
 }
